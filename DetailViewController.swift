9eb92585142d7047202cc481e0dc001b20d84ca6
Drew Sullivan <dsullivan@dev-030416.local>
2018-08-06 16:12:53 -0400

    Setting up detail page

---
+++ DetailViewController.swift
@@ -0,0 +1,13 @@
+//
+//  DetailViewController.swift
+//  Homepwner
+//
+//  Created by Drew Sullivan on 8/6/18.
+//  Copyright © 2018 Drew Sullivan. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController : UIViewController {
+    
+}
