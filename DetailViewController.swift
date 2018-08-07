326c9ae9c43ffeee42704b32aaed9fc7f8a7683f
Drew Sullivan <drew.sullivan.dma@gmail.com>
2018-08-06 21:44:17 -0400

    Finished ch. 13

--- DetailViewController.swift
+++ DetailViewController.swift
@@ -10,4 +10,31 @@ import UIKit
 
 class DetailViewController : UIViewController {
     
+    @IBOutlet var nameField: UITextField!
+    @IBOutlet var serialNumberField: UITextField!
+    @IBOutlet var valueField: UITextField!
+    @IBOutlet var dateLabel: UILabel!
+    
+    var item: Item! //Reference to the item that was clicked from the stack view
+    let numberFormatter: NumberFormatter = {
+        let nf = NumberFormatter()
+        nf.numberStyle = .decimal
+        nf.minimumFractionDigits = 2
+        nf.maximumFractionDigits = 2
+        return nf
+    }()
+    let dateFormatter: DateFormatter = {
+        let df = DateFormatter()
+        df.dateStyle = .medium
+        df.timeStyle = .none
+        return df
+    }()
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        nameField.text = item.name
+        serialNumberField.text = item.serialNumber
+        valueField.text = numberFormatter.string(from: NSNumber(value: item.valueInDollars))
+        dateLabel.text = dateFormatter.string(from: item.dateCreated)
+    }
 }
